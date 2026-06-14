4caf9402c4595442ce3656707fa3c68111e796b5
Azrael <dkolya2527@gmail.com>
2026-06-15 01:19:34 +0300

    написал команды из пункта 3: clear_data, structure, inside_generator, inside_reporter

--- run.sh
+++ run.sh
@@ -15,4 +15,21 @@ elif [ "$1" = "build_reporter" ]; then
 elif [ "$1" = "run_generator" ]; then
   docker run -v "$PWD/data:/data" reporter
 
+elif [ "$1" = "structure" ]; then
+  tree /F
 
+elif [ "$1" = "clear_data" ]; then
+  rm data/*.csv
+  rm data/*.html
+
+elif [ "$1" = "inside_generator" ]; then
+  docker run -v "$PWD/data:/data" generator
+  ls /data
+
+elif [ "$1" = "inside_reporter" ]; then
+  docker run -v "$PWD/data:/data" reporter
+  ls /data
+
+else
+  echo "Неправильная команда"
+fi
