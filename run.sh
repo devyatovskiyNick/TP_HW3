841b50a9b4d1badb06e562c6299f929ec886f129
Azrael <dkolya2527@gmail.com>
2026-06-15 01:13:21 +0300

    написал команды для reportera

--- run.sh
+++ run.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 if [ "$1" = "build_generator" ]; then
-  docker build -t generate ./generator
+  docker build -t generator ./generator
 
 elif [ "$1" = "run_generator" ]; then
   docker run -v "$PWD/data:/data" generate
 
 elif [ "$1" = "create_local_data" ]; then
   python generator/generate.py ./local_data
+
+elif [ "$1" = "build_reporter" ]; then
+  docker build -t reporter ./reporter
+
+elif [ "$1" = "run_generator" ]; then
+  docker run -v "$PWD/data:/data" reporter
+
+
