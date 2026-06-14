a81e34f9cb7b6f4829cc0bbf327840e405c8ff4b
Azrael <dkolya2527@gmail.com>
2026-06-15 01:26:47 +0300

    исправил команду run_generator, ссылалась на неправильное имя образа

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ if [ "$1" = "build_generator" ]; then
   docker build -t generator ./generator
 
 elif [ "$1" = "run_generator" ]; then
-  docker run -v "$PWD/data:/data" generate
+  docker run -v "$PWD/data:/data" generator
 
 elif [ "$1" = "create_local_data" ]; then
   python generator/generate.py ./local_data
