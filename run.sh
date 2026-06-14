86383cdcc44dba292bdc8d83c606ee13eed50bfe
Devyatovskiy Nick <108174060+devyatovskiyNick@users.noreply.github.com>
2026-06-15 01:48:20 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ if [ "$1" = "build_generator" ]; then
   docker build -t generator ./generator
 
 elif [ "$1" = "run_generator" ]; then
-  docker run -v "$PWD/data:/data" generator
+  docker run -v "./data:/data" generator
 
 elif [ "$1" = "create_local_data" ]; then
   python generator/generate.py ./local_data
@@ -13,7 +13,7 @@ elif [ "$1" = "build_reporter" ]; then
   docker build -t reporter ./reporter
 
 elif [ "$1" = "run_reporter" ]; then
-  docker run -v "$PWD/data:/data" reporter
+  docker run -v "./data:/data" reporter
 
 elif [ "$1" = "structure" ]; then
   tree /F
@@ -23,12 +23,12 @@ elif [ "$1" = "clear_data" ]; then
   rm data/*.html
 
 elif [ "$1" = "inside_generator" ]; then
-  docker run -v "$PWD/data:/data" generator
-  ls /data
+  docker run -v "./data:/data" generator
+  ls data/
 
 elif [ "$1" = "inside_reporter" ]; then
-  docker run -v "$PWD/data:/data" reporter
-  ls /data
+  docker run -v "./data:/data" reporter
+  ls data/
 
 else
   echo "Неправильная команда"
